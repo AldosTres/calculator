e1fdadbdbdcaa50008912dd9f75a8ef432d0272e
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-06-30 15:35:39 +0200

    feat: added button visual feedback when activated through the keyboard

--- src/index.js
+++ src/index.js
@@ -20,5 +20,17 @@ function handleKeyboardInput(event) {
 
   if (!validInput) return;
 
+  showButtonFeedback(validInput);
   processCalculatorInput(validInput);
+}
+
+function showButtonFeedback(buttonId) {
+  const button = document.getElementById(buttonId);
+  if (!button) return;
+
+  button.classList.add('is-keyboard-pressed');
+
+  setTimeout(() => {
+    button.classList.remove('is-keyboard-pressed');
+  }, 150);
 }
